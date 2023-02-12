db82a76b846cfe78f06c5735e7cb809287f6cec6
bercsenyirihi <70903145+bercsenyirihi@users.noreply.github.com>
2023-02-12 21:36:14 +0100

    Added files for 'pipemessage'

---
+++ pipemessage/PipeMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PipeMessages
+{
+    [Serializable]
+    public class PipeMessage
+    {
+        public PipeMessage()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public ActionType Action { get; set; }
+        public string BaseToken { get; set; }
+        public string QuoteToken { get; set; }
+        public string List { get; set; }
+    }
+
+    public enum ActionType
+    {
+        GetBaseTokenList,
+        GetQuoteTokenList,
+        GetCurrentData,
+        ReturnBaseTokenList,
+        ReturnQuoteTokenList,
+        ReturnCurrentData
+    }
+}
